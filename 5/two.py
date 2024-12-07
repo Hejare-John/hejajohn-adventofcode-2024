b2e330ffa650f4558443195a48b29012757af746
John Turesson <john@hejare.se>
2024-12-07 16:12:42 +0100

    Day 5

--- 5/two.py
+++ 5/two.py
@@ -21,6 +21,8 @@ for line in input_lines[:input_divider_index]:
   b = int(match[2])
   page_order[a].append(b)
 
+###
+
 def get_middle(update):
   assert(len(update) % 2 == 1)
   return update[int((len(update) - 1) / 2)]
@@ -30,19 +32,34 @@ def check_update_page(update, index):
   pages = page_order[update[index]]
   for i in pages:
     if i in update_head:
-      return False
-  return True
+      return update_head.index(i)
+  return -1
 
 def check_update(update):
   for i in range(len(update)):
-    if not check_update_page(update, i):
+    if check_update_page(update, i) != -1:
       return False
   return True
+
+def rearrange_update(update):
+  new_update = update.copy()
+  for i in range(len(new_update)):
+    while True:
+      j = check_update_page(new_update, i)
+      if j == -1:
+        break
+      a = new_update[i]
+      new_update.remove(a)
+      new_update.insert(j, a)
+  return new_update
 
 update_score = 0
 
 for update in updates:
-  if check_update(update):
-    update_score += get_middle(update)
+  middle = 0
+  while not check_update(update):
+    update = rearrange_update(update)
+    middle = get_middle(update)
+  update_score += middle
 
 print(update_score)
